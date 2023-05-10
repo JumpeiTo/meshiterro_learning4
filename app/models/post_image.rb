8f87732f26c6749f608110ce34a773433afa655c
EC2 Default User <ec2-user@ip-172-31-9-250.ap-northeast-1.compute.internal>
2023-05-10 11:41:09 +0000

    [finish]4-10

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -6,21 +6,12 @@ class PostImage < ApplicationRecord
     belongs_to :user
     
     
-    def new
-        @post_image = PostImage.new
-    end
-    
-    def create
-        @post_image = PostImage.new(post_image_params)
-        @post_image.user_id = current_user.id
-        @post_image.save
-        redirect_to post_images_path
-    end
-    
-    
-    private
-    
-    def post_image_params
-        params.require(:post_image).permit(:shop_name, :image, :caption)
+    def get_image
+        unless image.attached?
+            file_path = Rails.root.join('app/assets/images/no_image.jpg')
+            image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+        else
+            image
+        end
     end
 end
